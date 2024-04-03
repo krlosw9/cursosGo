c1609214c5d4fc38436c8abfd16ac352bdd8b270
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-03 11:58:24 -0500

    Programacion-golang | Arrays y Slices

--- basico/src/main.go
+++ basico/src/main.go
@@ -181,4 +181,30 @@ func main() {
 		}
 		fmt.Println(i)
 	}
+
+	// Array -> son inmutables y se les define cantidad de elementos que va a tener desde el inicio (son mas eficientes)
+	fmt.Println() // salto de linea
+	var array [4]int
+	fmt.Println(array) // [0 0 0 0]
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array)) // [1 2 0 0] 4(longitud del array) 4(capacidad del array)
+	// Slice -> son mutables, no requieren que se defina inicialmente la cantidad de elementos que va a tener
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
+
+	// Metodos en slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice) // [0 1 2 3 4 5 6 7]
+
+	// Append nueva lista
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice) // [0 1 2 3 4 5 6 7 8 9 10]
 }
