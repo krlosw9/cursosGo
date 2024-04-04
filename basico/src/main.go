f8a1e080e8e2377d0a4ab23ac5fa59510b34538f
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-03 20:14:40 -0500

    Programacion-golang | Range, Close y Select en channels

--- basico/src/main.go
+++ basico/src/main.go
@@ -2,14 +2,41 @@ package main
 
 import "fmt"
 
-func say(text string, c chan<- string) {
+func message(text string, c chan<- string) {
 	c <- text
 }
 
 func main() {
-	c := make(chan string, 1)
+	c := make(chan string, 2)
 
-	go say("Hello", c)
-	fmt.Println(<-c)
-	fmt.Println("World")
+	c <- "Mensaje1"
+	c <- "Mensaje2"
+
+	fmt.Println(len(c), cap(c))
+
+	close(c)
+
+	for v := range c {
+		fmt.Println(v)
+	}
+
+	// Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("Mensaje1 ", email1)
+	go message("Mensaje2 ", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email ", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email ", m2)
+		}
+	}
+
+	close(email1)
+	close(email2)
+
 }
