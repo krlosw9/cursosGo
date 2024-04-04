8e378d2b008db9594ad87c927505345d1f7e84e0
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-04 15:51:07 -0500

    golang-intermedio | Métodos y funciones

--- intermedio/poo/structs/class.go
+++ intermedio/poo/structs/class.go
@@ -7,10 +7,30 @@ type Employee struct {
 	name string
 }
 
+// visible function
+func (e *Employee) setId(id int) {
+	e.id = id
+}
+
+func (e *Employee) setName(name string) {
+	e.name = name
+}
+
+func (e *Employee) getId() int {
+	return e.id
+}
+
+func (e *Employee) getName() string {
+	return e.name
+}
+
 func main() {
 	e := Employee{}
 	fmt.Println(e)
 	e.id = 1
 	e.name = "Name"
+	e.setId(2)
+	e.setName("Name2")
 	fmt.Println(e)
+	fmt.Printf("id: %d, Name: %s\n", e.getId(), e.getName())
 }
