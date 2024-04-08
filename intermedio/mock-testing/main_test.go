5c51cbc783807dcd6fbba10352ba0ee731be6e1d
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-08 18:31:34 -0500

    golang-intermedio | Implementando Mocks

--- intermedio/mock-testing/main_test.go
+++ intermedio/mock-testing/main_test.go
@@ -10,24 +10,54 @@ func TestGetFullTimeEmployeeById(t *testing.T) {
 		expectedEmployee FullTimeEmployee
 	}{
 		{
-			id: 1,
+			id:  1,
 			dni: "1",
 			mockFunc: func() {
 				GetEmployeeById = func(id int) (Employee, error) {
 					return Employee{
-						id: 1,
+						id:       1,
 						Position: "CEO",
 					}, nil
 				}
 
 				GetPersonByDNI = func(dni string) (Person, error) {
 					return Person{
-						DNI: "1",
+						DNI:  "1",
 						Name: "John Doe",
-						Age: 35,
+						Age:  35,
 					}, nil
 				}
 			},
+			expectedEmployee: FullTimeEmployee{
+				Person: Person{
+					Age:  35,
+					DNI:  "1",
+					Name: "John Doe",
+				},
+				Employee: Employee{
+					id:       1,
+					Position: "CEO",
+				},
+			},
+		},
+	}
+
+	originalGetEmployeeById := GetEmployeeById
+	originalGetPersonByDNI := GetPersonByDNI
+
+	for _, test := range table {
+		test.mockFunc()
+		ft, err := GetFullTimeEmployeeById(test.id, test.dni)
+
+		if err != nil {
+			t.Errorf("Error when getting Employee")
+		}
+
+		if ft.Age != test.expectedEmployee.Age {
+			t.Errorf("GetFullTimeEmployeeById was incorrect, got %d expected %d", ft.Age, test.expectedEmployee.Age)
 		}
 	}
+
+	GetEmployeeById = originalGetEmployeeById
+	GetPersonByDNI = originalGetPersonByDNI
 }
