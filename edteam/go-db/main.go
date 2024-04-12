b673aeb9df1ca67244c4645e90172bc42f6851af
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-12 12:04:05 -0500

    database-go | 2.4 - Migraciones. (Parte 1)

--- edteam/go-db/main.go
+++ edteam/go-db/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"log"
+
+	"github.com/krlosw9/cursosGo/go-db/pkg/product"
 	"github.com/krlosw9/cursosGo/go-db/storage"
 )
 
 func main() {
 	storage.NewPostgresDB()
+
+	storageProduct := storage.NewPsqlProduct(storage.Pool())
+	serviceProduct := product.NewService(storageProduct)
+
+	if err := serviceProduct.Migrate(); err != nil {
+		log.Fatalf("product.Migrate: %v", err)
+	}
 }
--- edteam/go-db/pkg/product/product.go
+++ edteam/go-db/pkg/product/product.go
@@ -16,9 +16,25 @@ type Model struct {
 type Models []*Model
 
 type Storage interface {
-	Create(*Model) error
-	Update(*Model) error
-	GetAll() (Models, error)
-	GetByID(uint) (Model, error)
-	Delete(uint) error
+	Migrate() error
+	// Create(*Model) error
+	// Update(*Model) error
+	// GetAll() (Models, error)
+	// GetByID(uint) (Model, error)
+	// Delete(uint) error
+}
+
+// Service of product
+type Service struct {
+	storage Storage
+}
+
+// NewService return a pointer of Service
+func NewService(s Storage) *Service {
+	return &Service{s}
+}
+
+// Migrate is used for migrate product
+func (s *Service) Migrate() error {
+	return s.storage.Migrate()
 }
