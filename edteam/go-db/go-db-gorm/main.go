42c84aa5ffebafffb7b97b16fff1a32682acb49e
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-16 12:26:19 -0500

    database-go | 4.4 - Migraciones

--- edteam/go-db/go-db-gorm/main.go
+++ edteam/go-db/go-db-gorm/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/krlosw9/cursosGo/go-db-gorm/storage"
+import (
+	"github.com/krlosw9/cursosGo/go-db-gorm/model"
+	"github.com/krlosw9/cursosGo/go-db-gorm/storage"
+)
 
 func main() {
-	driver := storage.MySQL
+	driver := storage.Postgres
 	storage.New(driver)
+
+	storage.DB().AutoMigrate(&model.Product{}, &model.InvoiceHeader{}, &model.InvoiceItem{})
 
 }
