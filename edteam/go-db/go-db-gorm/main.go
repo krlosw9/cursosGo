07e0a6f73b6f3281902114ec92744535dbe6e6fd
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-16 15:01:34 -0500

    database-go | 4.8 - UPDATE

--- edteam/go-db/go-db-gorm/main.go
+++ edteam/go-db/go-db-gorm/main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/krlosw9/cursosGo/go-db-gorm/model"
 	"github.com/krlosw9/cursosGo/go-db-gorm/storage"
 )
 
 func main() {
-	driver := storage.Postgres
+	driver := storage.MySQL
 	storage.New(driver)
 
 	myProduct := model.Product{}
-	storage.DB().First(&myProduct, 3)
-	fmt.Println(myProduct)
+	myProduct.ID = 3
+
+	storage.DB().Model(&myProduct).Updates(
+		model.Product{Name: "Curso de Java", Price: 120},
+	)
 
 }
