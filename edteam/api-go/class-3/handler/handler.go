16460a985b27386e29fe302d652afa7acb939e71
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-20 18:59:13 -0500

    api-go | 3.5 - Handler Create

---
+++ edteam/api-go/class-3/handler/handler.go
@@ -0,0 +1,11 @@
+package handler
+
+import "github.com/krlosw9/cursosGo/api-go/class-3/model"
+
+type Storage interface {
+	Create(person *model.Person) error
+	Update(ID int, person *model.Person) error
+	Delete(ID int) error
+	GetByID(ID int) (model.Person, error)
+	GetAll() (model.Persons, error)
+}
---
+++ edteam/api-go/class-3/handler/person.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/krlosw9/cursosGo/api-go/class-3/model"
+)
+
+type person struct {
+	storage Storage
+}
+
+func newPerson(storage Storage) person {
+	return person{storage}
+}
+
+func (p *person) create(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Content-Type", "aplication/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message_type": "error", "message": "Método no permitido"}`))
+		return
+	}
+
+	data := model.Person{}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.Header().Set("Content-Type", "aplication/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"message_type": "error", "message": "La persona no tiene una estructura correcta"}`))
+		return
+	}
+
+	err = p.storage.Create(&data)
+	if err != nil {
+		w.Header().Set("Content-Type", "aplication/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message_type": "error", "message": "Hubo un problema al crear la persona"}`))
+		return
+	}
+
+	w.Header().Set("Content-Type", "aplication/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message_type": "message", "message": "Persona creada correctamente"}`))
+
+}
