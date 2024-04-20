9e679f28ef9b4b85746f4985e99684b269b2a9a5
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-20 18:13:36 -0500

    api-go | 3.3 - Storage update and delete

--- edteam/api-go/class-3/model/model.go
+++ edteam/api-go/class-3/model/model.go
@@ -3,5 +3,6 @@ package model
 import "errors"
 
 var (
-	ErrPersonCanNotBeNil = errors.New("la persona no puede ser null")
+	ErrPersonCanNotBeNil    = errors.New("la persona no puede ser null")
+	ErrIDPersonDoesNotExist = errors.New("persona no existe")
 )
--- edteam/api-go/class-3/storage/memory.go
+++ edteam/api-go/class-3/storage/memory.go
@@ -1,6 +1,10 @@
 package storage
 
-import "github.com/krlosw9/cursosGo/api-go/class-3/model"
+import (
+	"fmt"
+
+	"github.com/krlosw9/cursosGo/api-go/class-3/model"
+)
 
 // Memory
 type Memory struct {
@@ -24,6 +28,30 @@ func (m *Memory) Create(person *model.Person) error {
 
 	m.currentID++
 	m.Persons[m.currentID] = *person
+
+	return nil
+}
+
+func (m *Memory) Update(ID int, person *model.Person) error {
+	if person == nil {
+		return model.ErrPersonCanNotBeNil
+	}
+
+	if _, ok := m.Persons[ID]; !ok {
+		return fmt.Errorf("ID: %d: %w", ID, model.ErrIDPersonDoesNotExist)
+	}
+
+	m.Persons[ID] = *person
+
+	return nil
+}
+
+func (m *Memory) Delete(ID int) error {
+	if _, ok := m.Persons[ID]; !ok {
+		return fmt.Errorf("ID: %d: %w", ID, model.ErrIDPersonDoesNotExist)
+	}
+
+	delete(m.Persons, ID)
 
 	return nil
 }
