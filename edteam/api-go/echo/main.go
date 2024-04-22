e862e97d861c010ab5d71b6a3262c3f60c7abbe8
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-22 15:29:03 -0500

    api-go | 6.2 - Creando un servidor con Echo y uso del middleware Recover

--- edteam/api-go/echo/main.go
+++ edteam/api-go/echo/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Recover())
+
 	e.GET("/", saludar)
+	e.GET("/dividir", dividir)
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func saludar(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]string{"saludo": "Hello, World!"})
+}
+
+func dividir(c echo.Context) error {
+	d := c.QueryParam("d")
+	f, _ := strconv.Atoi(d)
+	if f == 0 {
+		return c.String(http.StatusBadRequest, "El valor no puede ser cero")
+	}
+	r := 3000 / f
+
+	return c.String(http.StatusOK, strconv.Itoa(r))
 }
