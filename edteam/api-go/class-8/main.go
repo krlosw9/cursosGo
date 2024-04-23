5d8fa5a03103cb4feb902afc020f4f5674eee079
Carlos Waldo Rojas <krlosw9@gmail.com>
2024-04-23 14:50:56 -0500

    api-go | 8.2 - Helper cliente

---
+++ edteam/api-go/class-8/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+}
+
+func httpClient(method, url string, body io.Reader) *http.Response {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		log.Fatalf("Request: %v", err)
+	}
+
+	client := http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Request: %v", err)
+	}
+
+	return response
+}
